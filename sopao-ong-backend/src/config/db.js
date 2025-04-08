0df72afe0846ed0141e6c918e9da0bd8e602c9b9
rafvmaia <rafvmaia@gmail.com>
2025-04-08 13:03:23 -0300

    Update db.js

--- sopao-ong-backend/src/config/db.js
+++ sopao-ong-backend/src/config/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Desativa a verificação do certificado (comum em provedores hospedados)
+  },
 });
 
 module.exports = pool;
